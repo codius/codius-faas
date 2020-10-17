a279a2b6aa325a65321de0b61950ca75a6e2ce65
Brandon Wilson <brandon@coil.com>
2020-10-16 23:55:49 -0500

    apply patches as yaml

--- install.sh
+++ install.sh
@@ -6,22 +6,16 @@ kubectl set env -n openfaas-fn deploy/system-github-event validate_customers=fal
 
 kubectl set env -n openfaas deploy/edge-router auth_url=http://edge-auth.openfaas:8080
 
-
 kubectl apply -f https://raw.githubusercontent.com/wilsonianb/faas-netes/master/artifacts/crds/openfaas.com_profiles.yaml
 kubectl patch -n openfaas deploy/gateway -p '
-{
-  "spec": {
-    "template": {
-      "spec": {
-        "containers": [{
-          "name": "faas-netes",
-          "image": "wilsonianbcoil/faas-netes",
-          "imagePullPolicy": "Always"
-        }]
-      }
-    }
-  }
-}'
+spec:
+  template:
+    spec:
+      containers:
+      - name: faas-netes
+        image: wilsonianbcoil/faas-netes
+        imagePullPolicy: Always
+'
 
 kubectl patch serviceaccount -n openfaas-fn default -p '{"automountServiceAccountToken": false}'
 
@@ -36,80 +30,54 @@ cp $GOPATH/src/github.com/openfaas-incubator/ofc-bootstrap/tmp/pub-cert.pem base
 kubectl apply -k .
 
 kubectl patch -n openfaas-fn deploy/buildshiprun -p '
-{
-  "spec": {
-    "template": {
-      "spec": {
-        "containers": [{
-          "name": "buildshiprun",
-          "image": "wilsonianbcoil/of-buildshiprun",
-          "imagePullPolicy": "Always",
-          "env": [{
-            "name": "profile",
-            "value": "ofc-workload"
-          }]
-        }]
-      }
-    }
-  }
-}'
+spec:
+  template:
+    spec:
+      containers:
+      - name: buildshiprun
+        image: wilsonianbcoil/of-buildshiprun
+        imagePullPolicy: Always
+        env:
+        - name: profile
+          value: ofc-workload
+'
 
 kubectl patch -n openfaas-fn deploy/system-dashboard -p '
-{
-  "spec": {
-    "template": {
-      "spec": {
-        "containers": [{
-          "name": "system-dashboard",
-          "image": "wilsonianbcoil/of-cloud-dashboard",
-          "imagePullPolicy": "Always",
-          "env": [{
-            "name": "github_app_url",
-            "valueFrom": {
-              "configMapKeyRef": {
-                "key": "github_app_url",
-                "name": "system-dashboard"
-              }
-            }
-          },
-          {
-            "name": "receipts_url",
-            "valueFrom": {
-              "configMapKeyRef": {
-                "key": "receipts_url",
-                "name": "system-dashboard"
-              }
-            }
-          },
-          {
-            "name": "payment_pointer",
-            "valueFrom": {
-              "configMapKeyRef": {
-                "key": "payment_pointer",
-                "name": "system-dashboard"
-              }
-            }
-          }],
-          "volumeMounts": [{
-            "name": "pubcert",
-            "mountPath": "/home/app/function/dist/pub-cert.pem",
-            "subPath": "pub-cert.pem"
-          }]
-        }],
-        "volumes": [{
-          "name": "pubcert",
-          "configMap": {
-            "name": "system-dashboard",
-            "items": [{
-              "key": "pub-cert.pem",
-              "path": "pub-cert.pem"
-            }]
-          }
-        }]
-      }
-    }
-  }
-}'
+spec:
+  template:
+    spec:
+      containers:
+      - name: system-dashboard
+        image: wilsonianbcoil/of-cloud-dashboard
+        imagePullPolicy: Always
+        env:
+        - name: github_app_url
+          valueFrom:
+            configMapKeyRef:
+              key: github_app_url
+              name: system-dashboard
+        - name: receipts_url
+          valueFrom:
+            configMapKeyRef:
+              key: receipts_url
+              name: system-dashboard
+        - name: payment_pointer
+          valueFrom:
+            configMapKeyRef:
+              key: payment_pointer
+              name: system-dashboard
+        volumeMounts:
+        - name: pubcert
+          mountPath: /home/app/function/dist/pub-cert.pem
+          subPath: pub-cert.pem
+      volumes:
+      - name: pubcert
+        configMap:
+          name: system-dashboard
+          items:
+          - key: pub-cert.pem
+            path: pub-cert.pem
+'
 
 export PASSWORD=$(kubectl get secret -n openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode; echo)
 
